0461a7e906c6283b454955a3398e762886c423a5
Nazir <n_ahmad20@live.co.uk>
2020-06-30 18:11:53 +0100

    Fetch monsters data from an api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends Component {
+
+  state = {
+    monsters: []
+  }
+
+  componentDidMount(){
+    axios("https://jsonplaceholder.typicode.com/users").then(res =>{
+      this.setState({
+        monsters: res.data
+      })
+    })
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+       this.state.monsters.map(m =>(
+          <h1 key={m.id}>{m.name}</h1>
+       ))
+     }
     </div>
   );
+  }
 }
 
 export default App;
