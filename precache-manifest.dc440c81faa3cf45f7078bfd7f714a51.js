945997eda89d80d6920fc2d8ec9d57878d79b768
Nazir <n_ahmad20@live.co.uk>
2020-07-01 13:48:46 +0100

    Updates

---
+++ precache-manifest.dc440c81faa3cf45f7078bfd7f714a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3b2da3a2fdcef653cd5fa77965ccd2",
+    "url": "/Simple-Monsters-App-React/index.html"
+  },
+  {
+    "revision": "d6b9e2f1c0063aa302a0",
+    "url": "/Simple-Monsters-App-React/static/css/main.e6c2abe8.chunk.css"
+  },
+  {
+    "revision": "4387643eaf34bd539f03",
+    "url": "/Simple-Monsters-App-React/static/js/2.bde54a13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Monsters-App-React/static/js/2.bde54a13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b9e2f1c0063aa302a0",
+    "url": "/Simple-Monsters-App-React/static/js/main.f2dbb144.chunk.js"
+  },
+  {
+    "revision": "6b43befeccc8a561a52e",
+    "url": "/Simple-Monsters-App-React/static/js/runtime-main.bc523da1.js"
+  }
+]);
